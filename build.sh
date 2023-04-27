703bb48a9e69e60fe0f266f6be9ef3a06b1a3513
William Quintal <william95quintalwilliam@outlook.com>
2023-04-27 15:11:29 -0400

    workflows

--- build.sh
+++ build.sh
@@ -4,8 +4,28 @@
 #
 
 export SHA=$(git rev-parse --short HEAD)
-
 export VERSION=${VERSION:-$SHA}
 
 
-echo $VERSION
+linuxTarget=(arm64)
+macTarget=(arm64)
+
+
+function build() {
+        export GOOS=$1
+        export GOARCH=$2
+
+        export WD=./build/${GOOS}/${GOARCH}
+
+        mkdir -p $WD
+
+        go build -ldflags "-X git.tmaws.io/tmconnect/logexplorer/cmd.sha1ver=${VERSION}" \
+                -o ${WD}/logexplorer
+}
+
+
+build "linux" "arm64"
+build "linux" "amd64"
+build "darwin" "arm64"
+build "darwin" "amd64"
+
